a59b6d03c33c50e2491608a9e6167acad5fb8df3
Matheus Rangel <matheus_rangel@ufrn.edu.br>
2019-10-06 15:36:07 -0300

    Comentando o codigo

--- guarda.py
+++ guarda.py
@@ -4,37 +4,55 @@ import os
 import hashlib
 import shutil
 
+
 class GuardaHash:
     def __init__(self, password: str = ""):
         self.password = password
 
+    # Faz o hash do array de bytes
     def hash(self, b: bytes):
         m = hashlib.sha256()
         m.update(b)
+        # Append da senha caso a opção escolhida seja hmac
         m.update(self.password.encode('utf-8'))
-        return m.digest().hex()
+        # Calcula o hash e salva o resultado em uma string no formato hexadecimal
+        return m.hexdigest()
 
 
 class Guarda:
-    def __init__(self, guarda_hash: GuardaHash, dir_path):
+    def __init__(self, guarda_hash, dir_path):
+        # Objeto de hash que será utilizado pelo Guarda
         self._guarda_hash = guarda_hash
+        # Caminho do diretorio que deve ser verificado
         self._dir_path = dir_path
+        # Caminho do arquivo onde a hash dos arquivos será armazenada
         self._hash_path = os.path.join(self.update_guardadir(), 'guarda.json')
 
     def inspect(self):
+        """
+        Inspeciona os arquivos presentes no diretorio e salva a hash de cada arquivo
+        """
         hashs = {}
+        # Iterando pelo diretorio
         for root, dirs, files in os.walk(self._dir_path):
+            # Ignorar a pasta criada pelo guarda onde a hash fica salva
             if root.startswith(os.path.join(self._dir_path, "guarda")):
                 continue
+            # Iterando sobre os arquivo presentes
             for file in files:
                 path_file = os.path.join(root, file)
+                # Calcula a hash do arquivo
                 h = self._guarda_hash.hash(open(path_file, mode='rb').read())
+                # Salva a hash no dicionario utilizando como chave o caminho do arquivo
                 hashs[path_file] = h
+        # Salva o dicionario com todas as hashs em um arquivo no formato json
         json.dump(hashs, open(self._hash_path, mode='w'), indent=4)
+        # Printando o resultado
         for key in hashs:
             print(key + ": " + hashs[key])
 
     def tracking(self):
+        # Le o arquivo de hash presente no diretorio
         files_hash = self.read_hash()
         result = ""
         for root, dirs, files in os.walk(self._dir_path):
@@ -43,13 +61,17 @@ class Guarda:
             for file in files:
                 file_path = os.path.join(root, file)
                 h = self._guarda_hash.hash(open(file_path, mode='rb').read())
+                # Verifica se o arquivo estáno dicionario.
                 if file_path in files_hash.keys():
                     old_h = files_hash[file_path]
                     files_hash.pop(file_path)
                     if h != old_h:
+                        # Caso a hash esteja diferente significa que o arquivo foi alterado
                         result += f"[ALTERADO] {file_path}\n"
                 else:
+                    # Caso o arquivo não esteja no dicionario significa que ele foi criado após a varredura
                     result += f"[NOVO] {file_path}\n"
+        # os arquivos no dicionario que não foram encontrados
         for deleted_file_path in files_hash.keys():
             result += f"[REMOVIDO] {deleted_file_path}\n"
         if not result:
